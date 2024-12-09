b95349ad796d27aa6db3d5a371de641c7fd3f644
Samno Khit <samnokhit@Samnos-MacBook-Pro.local>
2024-12-09 21:07:40 +0700

    inti project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <section>
+      <h2>Home Page abcf</h2>
+      <Button>Button</Button>
+    </section>
+  );
+}
